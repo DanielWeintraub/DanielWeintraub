a488888b8e010e9d8ea846168d87dd6a383ff89e
Dan Weintraub <93551197+DanielWeintraub@users.noreply.github.com>
2022-02-03 11:51:01 -0500

    new testfiles script

---
+++ s3testfiles/testfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+bucket=pd-rdc-asdf51-snd
+
+for i in $(seq 1 150); do
+       echo "testfile ${i}-$(date)"> "testfile-${i}.txt"
+       aws-okta exec rd-infra -- aws s3 cp "testfile-${i}.txt" "s3://${bucket}/"
+       if [[ $((i%2)) -eq 0 ]]; then
+	       aws-okta exec rd-infra -- aws s3 rm "s3://${bucket}/testfile-${i}.txt"
+       fi
+       rm "testfile-${i}.txt"
+done
+
